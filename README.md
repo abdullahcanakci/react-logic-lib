885a7ce4caceb0c2a309de37fd04b25cbaaba38c
Abdullah Çanakçı <abdullahcanakci.0@gmail.com>
2021-12-18 13:45:47 +0300

    readme check on gh

---
+++ README.md
@@ -0,0 +1,1 @@
+A collection of components mainly to be used for logic, since styles are rough and won't be suitable for most things.
--- src/Layout/Layout.tsx
+++ src/Layout/Layout.tsx
@@ -13,13 +13,22 @@ export default function Layout({ children, components }) {
         setToggled={setToggled}
         components={components}
       />
-      <div className="w-full overflow-y-auto grid place-items-center relative p-4 text-justify">
+      <div className="w-full overflow-y-auto grid place-items-center relative text-justify overflow-x-hidden">
         <button
           className="absolute top-0 left-0 lg:hidden w-12 h-12 p-4 m-2 rounded-full cursor-pointer bg-slate-800  shadow-sm hover:shadow-lg hover:bg-slate-900 transition-colors duration-150 text-white"
           onClick={() => setToggled((e) => !e)}
         >
           <FaBars className="w-full h-full" />
         </button>
+        <div className="grid place-items-center w-24 h-24  rotate-45 absolute top-0 right-0">
+          <a
+            href="https://github.com/abdullahcanakci/react-logic-lib"
+            className="w-48  ml-[-3rem] py-1 text-xs bg-red-400 text-center hover:bg-red-500  transition-colors duration-200shadow-sm hover:shadow-lg cursor-pointer"
+            target="_blank"
+          >
+            Check On Github
+          </a>
+        </div>
         <React.Suspense fallback={<>....</>}>
           <Routes>
             {components.map((component) => (
